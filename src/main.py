a88ee1a6125990622bf49b2c1e2b9ef3de641882
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-10-19 09:38:05 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import os
 import sys
 
  # import sys in python is loading the module named sys into the current 
-/# namespace so that you can access the functions and anything else defined within the module using the module name.
+ # namespace so that you can access the functions and anything else defined within the module using the module name.
 
 import cv2 as cv
 
