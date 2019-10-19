730b6a4a870bdfae3d289c9835484621b21f4389
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-10-19 09:29:34 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+import os
 
+// The OS module in python provides functions for interacting with the operating system
+
+import sys
+
+ // import sys in python is loading the module named sys into the current 
+// namespace so that you can access the functions and anything else defined within the module using the module name.
+
+import tensorflow as tf
+
+// 
+
+import cv2 as cv
+
+import numpy as np 
